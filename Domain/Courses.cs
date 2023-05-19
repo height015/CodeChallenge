2dfc73c9802be09a45c9843c8c19688bfbd9ec9d
joejoe <josephefunbote@gmail.com>
2023-05-19 21:01:35 +0100

    Entity Updated and Migration

--- Domain/Courses.cs
+++ Domain/Courses.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 using CodeChallenge.Helpers;
 
 namespace CodeChallenge.Domain;
@@ -11,8 +12,17 @@ public class Courses : BaseEntity
     {
         StudentCourses = new HashSet<StudentCourse>();
     }
+
+    [StringLength(50, MinimumLength = 2, ErrorMessage = "Course Name contains fewer or more characters than expected. (2 to 50 characters expected)")]
+    [Required(ErrorMessage = "Course Name is required", AllowEmptyStrings = false)]
     public string CourseName { get; set; }
+
+    [StringLength(50, MinimumLength = 2, ErrorMessage = "Course Title contains fewer or more characters than expected. (2 to 50 characters expected)")]
+    [Required(ErrorMessage = "Course Title is required", AllowEmptyStrings = false)]
     public string CourseTitle { get; set; }
+
+    [StringLength(10, MinimumLength = 2, ErrorMessage = "Course Code contains fewer or more characters than expected. (2 to 10 characters expected)")]
+    [Required(ErrorMessage = "Course Code is required", AllowEmptyStrings = false)]
     public string CourseCode { get; set; }
 
     public ICollection<StudentCourse> StudentCourses { get; set; }
